35b572f1f86e6b5f612e35b897bdfbad618c0e60
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-13 17:53:50 -0300

    Criando personagem e script para mover o chão

---
+++ teste/Assets/Scripts/MoveOffset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOffset : MonoBehaviour {
+
+	private Material currentMaterial;
+	private float offset;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += 0.001f;
+		currentMaterial.SetTextureOffset("_MainText", new Vector2(offset * speed, 0));
+	}
+}
