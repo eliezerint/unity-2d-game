1b5ade8f32503c574bf81348347d63831d1daf9e
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-14 00:05:38 -0300

    Adicionado plano de fundo

--- teste/Assets/Scripts/MoveOffset.cs
+++ teste/Assets/Scripts/MoveOffset.cs
@@ -10,12 +10,12 @@ public class MoveOffset : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		currentMaterial = GetComponent<Renderer>().material;
+		currentMaterial = GetComponent<Renderer> ().material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		offset += 0.001f;
-		currentMaterial.SetTextureOffset("_MainText", new Vector2(offset * speed, 0));
+		currentMaterial.SetTextureOffset("_MainTex", new Vector2(offset * speed, 0));
 	}
 }
