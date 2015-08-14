775ad6b66ba75a6aa2a07f0d1be8b82f952d0868
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-08-14 00:06:04 -0300

    Script de movimentação do personagem

---
+++ teste/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public GameObject player;
+	public float maxSceneHeight;
+	public float minSceneHeight;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tranlation = Input.GetAxis ("Vertical") * speed;
+		player.transform.Translate (0, tranlation, 0);
+
+		if (player.transform.position.y > maxSceneHeight) {
+			player.transform.position = new Vector3(0, maxSceneHeight, -10);
+		}
+
+		if (player.transform.position.y < minSceneHeight) {
+			player.transform.position = new Vector3(0, minSceneHeight, -10);
+		}
+	}
+}
